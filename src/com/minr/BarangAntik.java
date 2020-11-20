99c7b4f942b87acf25454ee9aa00cf9cf0331b5d
M Ikhlas N R <72897730+M-Ikhlas-N-R@users.noreply.github.com>
2020-11-20 17:25:11 +0700

    Add files via upload

---
+++ src/com/minr/BarangAntik.java
@@ -0,0 +1,13 @@
+package com.minr;
+
+public class BarangAntik {
+    private final int umur;
+
+    public BarangAntik(int umur) {
+        this.umur = umur;
+    }
+
+    public void tampilUmur() {
+        System.out.println("Umur barang antik ini adalah : " + this.umur + " tahun.");
+    }
+}
---
+++ src/com/minr/Main.java
@@ -0,0 +1,17 @@
+package com.minr;
+/*
+ * @author
+ * Nama              : M Ikhlas N R
+ * Kelas             : IF-1
+ * NIM               : 10119022
+ * Deskripsi Program : latihan inheritance
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        Radio radio = new Radio(234);
+        radio.setName("Radio AM");
+        System.out.println("Nama barang Antik : " + radio.getName());
+        radio.tampilUmur();
+    }
+}
